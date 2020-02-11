3316356ce442420401d2733bce3ac1eee40cf7d5
George Taray <Gtaray@Hotmail.com>
2020-02-11 06:51:20 -0800

    Initial work.

---
+++ campaign/scripts/char_cyberneticslist.lua
@@ -0,0 +1,70 @@
+-- 
+-- Please see the license.html file included with this distribution for 
+-- attribution and copyright information.
+--
+
+local sortLocked = false;
+
+function setSortLock(isLocked)
+	sortLocked = isLocked;
+end
+
+function onInit()
+	registerMenuItem(Interface.getString("list_menu_createitem"), "insert", 5);
+
+	local node = getDatabaseNode();
+end
+
+function onClose()
+	local node = getDatabaseNode();
+end
+
+function onMenuSelection(selection)
+	if selection == 5 then
+		addEntry(true);
+	end
+end
+
+function update()
+	local bEditMode = (window.inventorylist_iedit.getValue() == 1);
+	window.idelete_header.setVisible(bEditMode);
+	for _,w in ipairs(getWindows()) do
+		w.idelete.setVisibility(bEditMode);
+	end
+end
+
+function addEntry(bFocus)
+	local w = createWindow();
+	if w then
+		if bFocus then
+			w.name.setFocus();
+		end
+	end
+	return w;
+end
+
+function onClickDown(button, x, y)
+	return true;
+end
+
+function onClickRelease(button, x, y)
+	if not getNextWindow(nil) then
+		addEntry(true);
+	end
+	return true;
+end
+
+function onSortCompare(w1, w2)
+	if sortLocked then
+		return false;
+	end
+	return ItemManager.onInventorySortCompare(w1, w2);
+end
+
+function updateContainers()
+	ItemManager.onInventorySortUpdate(self);
+end
+
+function onDrop(x, y, draginfo)
+	return ItemManager.handleAnyDrop(window.getDatabaseNode(), draginfo);
+end
